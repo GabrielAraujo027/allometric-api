cf6f13a532dc9d4014c86446bde1045fc4f25766
Gabriel Araújo Lira <gabrielaraujolira12@gmail.com>
2026-03-14 20:30:38 -0300

    Create models.py

---
+++ app/models.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class GrupoAnimal(str, Enum):
+    passeriforme = "passeriforme"
+    nao_passeriforme = "nao_passeriforme"
+    mamifero_placentario = "mamifero_placentario"
+    marsupial = "marsupial"
+    reptil = "reptil"
