be2615002443097753ba726b6ec8b36225c11763
Gabriel Araújo Lira <gabrielaraujolira12@gmail.com>
2026-05-06 20:39:54 -0300

    Alter constants

--- app/constants.py
+++ app/constants.py
@@ -1,41 +1,25 @@
 GRUPOS = {
     "passeriforme": {
-        "peso_modelo": 0.03,
-        "dose_tratamento": 10,
-        "frequencia": 12,
-        "intervalo_tratamento": 5,
         "constante_metabolica": 129
     },
 
     "nao_passeriforme": {
-        "peso_modelo": 0.5,
-        "dose_tratamento": 8,
-        "frequencia": 24,
-        "intervalo_tratamento": 7,
         "constante_metabolica": 78
     },
 
     "mamifero_placentario": {
-        "peso_modelo": 10,
-        "dose_tratamento": 5,
-        "frequencia": 12,
-        "intervalo_tratamento": 5,
         "constante_metabolica": 70
     },
 
     "marsupial": {
-        "peso_modelo": 5,
-        "dose_tratamento": 4,
-        "frequencia": 24,
-        "intervalo_tratamento": 7,
         "constante_metabolica": 49
-    },
-
-    "reptil": {
-        "peso_modelo": 2,
-        "dose_tratamento": 3,
-        "frequencia": 48,
-        "intervalo_tratamento": 10,
-        "constante_metabolica": 10
     }
 }
+
+# MASSA_ANIMAIS_MODELO = {
+#     "gato": 4,
+#     "cachorro": 10,
+#     "ovino": 500,
+#     "Equino": 500,
+#     "homem": 70
+# }
